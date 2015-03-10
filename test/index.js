30590c3d7896acafb62433159b9d98ff7bde96c6
petecoop <pete@petecoop.co.uk>
2015-03-10 00:12:11 +0000

    test

--- test/index.js
+++ test/index.js
@@ -2,8 +2,21 @@ var Phantasma = require('../index');
 var should = require('should');
 
 describe('Phantasma', function () {
+
   it('should be constructable', function () {
     var ph = new Phantasma();
     ph.should.be.ok;
+    ph.exit();
   });
+
+  describe('Methods', function () {
+    
+    it('should open page', function (done) {
+      new Phantasma()
+        .open('https://petecoop.co.uk')
+        .exit()
+    });
+
+  });  
+
 });
