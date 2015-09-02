2b408c148ec0200cfe4b09fed84f4041f90d6639
MakoMakox <macovei.gabriel.razvan@gmail.com>
2015-09-02 14:38:45 +0300

    Added extractDomElement test method
    Testing the extractDomElement() it should save an image in temp folder named h1.png

--- test/index.js
+++ test/index.js
@@ -110,6 +110,15 @@ describe('Phantasma', function () {
         });
     });
 
+    it('should extract h1 as image', function () {
+      var path = 'test/temp/h1.png';
+      return ph.open('http://localhost:3000')
+        .extractDomElement('h1[id="heading"]',path)
+        .then(function () {
+          fs.existsSync(path).should.be.true;
+        });
+    });
+
     it('should navigate backwards and forwards', function () {
       return ph.open('http://localhost:3000')
         .open('http://localhost:3000/page1.html')
